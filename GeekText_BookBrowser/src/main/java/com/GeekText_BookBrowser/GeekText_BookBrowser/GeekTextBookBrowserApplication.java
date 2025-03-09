7b0c33d5fd416f4610cf3fa9a75d5aed246ff979
Mario Casas <MCasas548@gmail.com>
2025-03-09 05:53:26 -0400

    test

---
+++ GeekText_BookBrowser/src/main/java/com/GeekText_BookBrowser/GeekText_BookBrowser/GeekTextBookBrowserApplication.java
@@ -0,0 +1,13 @@
+package com.GeekText_BookBrowser.GeekText_BookBrowser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekTextBookBrowserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekTextBookBrowserApplication.class, args);
+	}
+
+}
---
+++ GeekText_BookBrowser/src/test/java/com/GeekText_BookBrowser/GeekText_BookBrowser/GeekTextBookBrowserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GeekText_BookBrowser.GeekText_BookBrowser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekTextBookBrowserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
