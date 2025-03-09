1008a5901dc0857b2de65eac3b6ed88619565626
Mario Casas <MCasas548@gmail.com>
2025-03-09 06:42:10 -0400

    first push

---
+++ demo/src/main/java/com/bookBrowser/GeekTextBookBrowser/demo/Service/BookService.java
@@ -0,0 +1,36 @@
+package com.bookBrowser.GeekTextBookBrowser.demo.Service;
+
+import com.bookBrowser.GeekTextBookBrowser.demo.Entity.BookEntity;
+import com.bookBrowser.GeekTextBookBrowser.demo.Repository.BookRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepo bookRepo;
+
+    public BookEntity saveDetails(BookEntity bookEntity) {
+
+        return bookRepo.save(bookEntity);
+
+    }
+
+    public BookEntity getBookDetailsById(int bookId) {
+
+        return bookRepo.findById(bookId).orElse(null);
+
+    }
+
+    public List<BookEntity> getBooksByGenre(int genreId) {
+        return bookRepo.findByGenreId(genreId);
+    }
+
+    public List<BookEntity> getTopSellingBooks() {
+        return bookRepo.findTop10ByOrderBySaleQuantityDesc();
+    }
+
+}
