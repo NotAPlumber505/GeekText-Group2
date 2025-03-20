fbc8ddec931e9361e1f533ae32ae4c0807fb0c6a
Mario Casas <MCasas548@gmail.com>
2025-03-20 01:38:20 -0400

    - Implemented GET request to retrieve books by rating - Implemented PUT request to discount books by publisher

--- demo/src/main/java/com/bookBrowser/GeekTextBookBrowser/demo/Repository/BookRepo.java
+++ demo/src/main/java/com/bookBrowser/GeekTextBookBrowser/demo/Repository/BookRepo.java
@@ -1,12 +1,8 @@
 package com.bookBrowser.GeekTextBookBrowser.demo.Repository;
 
 import com.bookBrowser.GeekTextBookBrowser.demo.Entity.BookEntity;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 
-import java.awt.print.Book;
 import java.util.List;
 
 public interface BookRepo extends JpaRepository<BookEntity, Integer> {
