be4f3b0196ffee1ba350df24b97d4193658f324d
YoctoZept <kcastil45.kc@gmail.com>
2025-03-22 02:51:33 -0400

    Delete ShoppingCart directory

--- ShoppingCart/src/main/java/ShoppingCart/ShoppingCart/Repository/BookRepo.java
+++
@@ -1,8 +0,0 @@
-package ShoppingCart.ShoppingCart.Repository;
-
-import ShoppingCart.ShoppingCart.Entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepo extends JpaRepository<Book, Integer> {
-
-}
--- ShoppingCart/src/main/java/ShoppingCart/ShoppingCart/Repository/CartRepo.java
+++
@@ -1,7 +0,0 @@
-package ShoppingCart.ShoppingCart.Repository;
-
-import ShoppingCart.ShoppingCart.Entity.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartRepo extends JpaRepository<Cart, Integer> {
-}
