345bf42fd6603287094aff4d5010e6b1db32fe9d
YoctoZept <kcastil45.kc@gmail.com>
2025-03-23 05:39:01 -0400

    Sprint 4 commit
    Deletion book and cart added

---
+++ ShoppingCart/src/main/java/ShoppingCart/ShoppingCart/Entity/Cart.java
@@ -0,0 +1,36 @@
+package ShoppingCart.ShoppingCart.Entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+
+@Entity
+@Data
+@Table(name = "Cart")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_id")
+    private int cartId;
+
+    @Column(name = "user_id", nullable = false)
+    private int userId;
+
+    @ManyToMany
+    @JsonManagedReference
+    @JoinTable(
+            name = "cart_books",
+            joinColumns = @JoinColumn(name = "cart_id"),
+            inverseJoinColumns = @JoinColumn(name = "book_id")
+    )
+    private List<Book> books;
+
+}
