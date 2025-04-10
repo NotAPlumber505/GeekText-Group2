859846326f0d69f31744446a7ce247e9fa05349c
YoctoZept <kcastil45.kc@gmail.com>
2025-04-10 03:16:17 -0400

    Delete src directory

--- src/main/java/com/Springpro/Springpro/Repository/BookRepo.java
+++
@@ -1,9 +0,0 @@
-package com.Springpro.Springpro.Repository;
-
-import com.Springpro.Springpro.Entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface BookRepo extends JpaRepository<Book, Integer> {
-}
--- src/main/java/com/springpro/Springpro/Repository/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package com.springpro.Springpro.Repository;
-
-import com.springpro.Springpro.Entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Integer> {
-}
